844a66554c0c56640ab27df236372235599cf093
Andre <andre2016999@gmail.com>
2025-02-04 13:38:18 -0300

    att App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Header } from './components/Header/Header.tsx';
+import { Task } from './components/Tasks/Tasks.tsx';
 import { Counter } from './components/Counter/Counter.tsx'
 
 import "./styles/global.css"
+
 
 function App() {
   return (
     <>
     <Header></Header>
-    <Counter></Counter>
+    <Task></Task>
     </>
   );
 }
