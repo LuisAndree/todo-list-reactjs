627ae6d1c9691e8ec69a031e3fbb0021ecc73d2d
Andre <andre2016999@gmail.com>
2025-02-14 13:50:15 -0300

    App att

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/Header/Header.tsx';
 import { Task } from './components/Tasks/Tasks.tsx';
 import { Counter } from './components/Counter/Counter.tsx'
@@ -7,10 +7,28 @@ import "./styles/global.css"
 
 
 function App() {
+
+  // const [toggle, setToggle] = useState(false)
+
+  // useEffect(() => {
+  //   console.log('executando a funcao do useEffect...')
+
+  //   //componentWillUnmount => cleanup Function
+  //   return () => {
+  //     //cleanup Function
+  //     console.log('essa funcao vai ser executada quando o componente App for desmontado da tela')
+  //   }
+  // }, [toggle])
+
+  // O useEfeect será desaarado sempre q alguma variavel do array de dependencias for alterada
+  // Por padrao sempre o useEffect sera disparada apos a montagem do componente (componentDidMount)
+
   return (
     <>
-    <Header></Header>
-    <Task></Task>
+      <Header></Header>
+      <Task></Task>
+
+      {/* <button onClick={()=> setToggle(!toggle) }>Toggle</button> */}
     </>
   );
 }
