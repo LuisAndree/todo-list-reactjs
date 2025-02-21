b0e33647c5dc64a4fe967ccb55f00b647a2c5ecb
Luís André <andre2016999@gmail.com>
2025-02-20 21:05:40 -0300

    feat: aprendendo useRefs

--- src/components/Concepts/Refs.tsx
+++ src/components/Concepts/Refs.tsx
@@ -1,12 +1,28 @@
-import React, {useRef} from "react"
+import { FormEvent, useRef, useState } from "react";
 
 export const Refs: React.FC = () => {
-    const ref = useRef(0); // { current: 0}
+    const inputNameRef = useRef<HTMLInputElement>(null);
+    const inputEmailRef = useRef<HTMLInputElement>(null);
+    const inputPasswordRef = useRef<HTMLInputElement>(null); // { current: 0}
+    const [toggle, setToggle] = useState(false);
+
+    function handleOnSubmit(event: FormEvent) {
+            event.preventDefault() // evita que a pagina seja recarregada ao submeter o formulario
+            console.log('submeteu');
+            console.log(inputNameRef.current?.value);
+            console.log(inputEmailRef.current?.value);
+            console.log(inputPasswordRef.current?.value);
+    }
 
     return (
-        <div>
+        <form style={{ padding: "2rem" }} onSubmit={(event) => handleOnSubmit(event)}>
             <h1>useRef</h1>
-            <p>{`O valor da Ref é ${ref.current}`}</p>
-        </div>
+            <br />
+            <input type="text" placeholder="Nome Completo" ref={inputNameRef} />
+            <input type="email" placeholder="E-mail" ref={inputEmailRef} />
+            <input type="password" placeholder="Senha" ref={inputPasswordRef} />
+            <br />
+            <button type="submit"> Submeter </button>
+        </form>
     );
 };
